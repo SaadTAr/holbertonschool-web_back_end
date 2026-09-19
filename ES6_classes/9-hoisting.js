22e3d1ccf1e13565bc8a101b14be9be6e8b0bec7
SaadTAr <saadalatar3445@gmail.com>
2026-09-19 20:47:52 +0000

    Fix task 9 listOfStudents export

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,6 @@
-export class HolbertonClass {
+const listOfStudents = [];
+
+class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -13,7 +15,7 @@ export class HolbertonClass {
   }
 }
 
-export class StudentHolberton {
+class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -36,10 +38,10 @@ export class StudentHolberton {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export const listOfStudents = [
-  new StudentHolberton('Guillaume', 'Salva', class2020),
-  new StudentHolberton('John', 'Doe', class2020),
-  new StudentHolberton('Albert', 'Clinton', class2019),
-  new StudentHolberton('Donald', 'Bush', class2019),
-  new StudentHolberton('Jason', 'Sandler', class2019),
-];
+listOfStudents.push(new StudentHolberton('Guillaume', 'Salva', class2020));
+listOfStudents.push(new StudentHolberton('John', 'Doe', class2020));
+listOfStudents.push(new StudentHolberton('Albert', 'Clinton', class2019));
+listOfStudents.push(new StudentHolberton('Donald', 'Bush', class2019));
+listOfStudents.push(new StudentHolberton('Jason', 'Sandler', class2019));
+
+export default listOfStudents;
